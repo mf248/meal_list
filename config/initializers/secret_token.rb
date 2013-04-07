a84f9d9387cb39227ceaa4addadb859c5103d26f
Michael Fergus <mfergus48@gmail.com>
2013-04-06 23:48:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MealList::Application.config.secret_token = 'e4402346dc4fbcab58f3090c46db05a55c930b84172f419cc3d71a61cb0c4440168a8d410ab7c016c78690ae98e25872110b1ae82514c7b9ab9f5bb204e4cc17'
